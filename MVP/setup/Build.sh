7530fff74ab8ecaeb52bc670f96a56e7c0f33461
Aaron Fry <afry@netsprout.com>
2019-03-19 15:08:34 -0700

    Add the new v3.1.9-p6 tag

--- MVP/setup/Build.sh
+++ MVP/setup/Build.sh
@@ -29,8 +29,8 @@ NC='\033[0m'        # Define default text
 EXTRACT=/home/pi/unpack  # Working directory for download and unzipping
 TARGET=/home/pi/MVP      # Location for MVP
 RELEASE=mvp              # Package (repository) to download
-VERSION=v3.1.9-p3        # github version to work with
-ZIP_DIR=3.1.9-p3
+VERSION=v3.1.9-p6        # github version to work with
+ZIP_DIR=3.1.9-p6
 GITHUB=https://github.com/netsprout/$RELEASE/archive/$VERSION.zip    # Address of Github archive
 
 echo $EXTRACT
